6c260c29568dffd5fc533359cabdd3b875437444
Pablo Domingues <pablodomingues198@gmail.com>
2025-06-18 15:29:17 -0300

    feat: starting first project in React

---
+++ PROJETOS/REACT/1_CalculadoraIMC/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImcCalc from './components/ImcCalc'
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container">
+      <ImcCalc></ImcCalc>
+    </div>
+  )
+}
+
+export default App

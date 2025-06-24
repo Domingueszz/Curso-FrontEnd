10736903dd499b186d7a68ef35fd4f2d1c5006eb
Pablo Domingues <pablodomingues198@gmail.com>
2025-06-24 15:42:08 -0300

    started in project

---
+++ PROJETOS/REACT/2_Forms/src/App.jsx
@@ -0,0 +1,33 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com sua compra, 
+          utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            <div className="inputd-container"></div>
+            <div className="actions">
+              <button type='button'>
+                <GrFormPrevious/>
+                <span>Voltar</span>
+              </button>
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext/>
+              </button>
+            </div>
+          </form>
+        </div>
+    </div>
+  )
+}
+
+export default App

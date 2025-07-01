be94cf2671c72443a06e0b68878c917f8857245c
Pablo Domingues <pablodomingues198@gmail.com>
2025-07-01 11:29:09 -0300

    feat: working in project

--- PROJETOS/REACT/React_JS/3_Quiz/src/components/Welcome.jsx
+++ PROJETOS/REACT/React_JS/3_Quiz/src/components/Welcome.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import "./Welcome.css"
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
 import Quiz from "../img/quiz.svg"
-
+import "./Welcome.css"
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
+
   return (
-    <div>
+    <div id='welcome'>
       <h2>Seja bem-vindo</h2>
       <p>CLique no botão abaixo para começar:</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({type: "CHANGE_STATE"})}>
+        Iniciar</button>
       <img src={Quiz} alt="Início do Quiz" />
     </div>
   )
